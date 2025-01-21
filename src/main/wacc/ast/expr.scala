00f340a4b496ef067da3ca9281d2c568582a7107
freddien03 <60051731+freddien03@users.noreply.github.com>
2025-01-21 18:23:49 +0000

    refactor: reduce Expr AST into one enum to avoid overhead

--- src/main/wacc/ast/expr.scala
+++ src/main/wacc/ast/expr.scala
@@ -3,20 +3,12 @@ package wacc.ast
 import parsley.generic.{ParserBridge1, ParserBridge2}
 
 enum Expr {
-    case Unary(u: UnaryOp)
-    case Binary(b: BinaryOp)
-    case Lit(a: Atom)
-}
-
-enum UnaryOp {
     case Not(e: Expr)
     case Neg(e: Expr)
     case Len(e: Expr)
     case Ord(e: Expr)
     case Chr(e: Expr)
-}
 
-enum BinaryOp {
     case Mul(x: Expr, y: Expr)
     case Div(x: Expr, y: Expr)
     case Mod(x: Expr, y: Expr)
@@ -30,9 +22,7 @@ enum BinaryOp {
     case NotEq(x: Expr, y: Expr)
     case And(x: Expr, y: Expr)
     case Or(x: Expr, y: Expr)
-}
 
-enum Atom {
     case IntLit(n: BigInt)
     case BoolLit(b: Boolean)
     case CharLit(c: Char)
@@ -43,18 +33,32 @@ enum Atom {
 }
 
 object Expr {
-    object Unary extends ParserBridge1[UnaryOp, Expr]
-    object Binary extends ParserBridge1[BinaryOp, Expr]
-    object Lit extends ParserBridge1[Atom, Expr]
-}
+    object Not extends ParserBridge1[Expr, Expr]
+    object Neg extends ParserBridge1[Expr, Expr]
+    object Len extends ParserBridge1[Expr, Expr]
+    object Ord extends ParserBridge1[Expr, Expr]
+    object Chr extends ParserBridge1[Expr, Expr]
 
-object Atom {
-    object IntLit extends ParserBridge1[BigInt, Atom]
-    object BoolLit extends ParserBridge1[Boolean, Atom]
-    object CharLit extends ParserBridge1[Char, Atom]
-    object StrLit extends ParserBridge1[String, Atom]
-    object Ident extends ParserBridge1[String, Atom]
-    object ArrayLit extends ParserBridge2[String, List[Expr], Atom]
+    object Mul extends ParserBridge2[Expr, Expr, Expr]
+    object Div extends ParserBridge2[Expr, Expr, Expr]
+    object Mod extends ParserBridge2[Expr, Expr, Expr]
+    object Add extends ParserBridge2[Expr, Expr, Expr]
+    object Sub extends ParserBridge2[Expr, Expr, Expr]
+    object Greater extends ParserBridge2[Expr, Expr, Expr]
+    object GreaterEq extends ParserBridge2[Expr, Expr, Expr]
+    object Less extends ParserBridge2[Expr, Expr, Expr]
+    object LessEq extends ParserBridge2[Expr, Expr, Expr]
+    object Eq extends ParserBridge2[Expr, Expr, Expr]
+    object NotEq extends ParserBridge2[Expr, Expr, Expr]
+    object And extends ParserBridge2[Expr, Expr, Expr]
+    object Or extends ParserBridge2[Expr, Expr, Expr]
+
+    object IntLit extends ParserBridge1[BigInt, Expr]
+    object BoolLit extends ParserBridge1[Boolean, Expr]
+    object CharLit extends ParserBridge1[Char, Expr]
+    object StrLit extends ParserBridge1[String, Expr]
+    object Ident extends ParserBridge1[String, Expr]
+    object ArrayLit extends ParserBridge2[String, List[Expr], Expr]
 }
 
 
