b9528432592b05f8e70ebce36fa704e922d325b1
Serhii Popov <Serhovski@users.noreply.github.com>
2025-01-21 21:16:39 +0000

    test: syntax-test outputs programs that failed parsing

--- src/test/wacc/parse/syntax/syntax_test.scala
+++ src/test/wacc/parse/syntax/syntax_test.scala
@@ -8,13 +8,14 @@ import wacc.parser
 val TEST_FILES = List("basic", "array", "expressions", "function", "if", "IO", "pairs", "runtimeErr", "scope", "sequence", "variables", "while" )
 class syntax_test extends AnyFlatSpec {
     TEST_FILES.foreach { path => 
-        path should "be able to parse " in {
-            val ran_tests = get_tests("valid/" + path).map(parser.parse(_))
-            ran_tests.forall { 
-                case parsley.Success(_) => true
-                case _        => false
-            } shouldBe true
+        path should "be able to parse all test cases" in {
+        val ranTests = get_tests(s"valid/$path").map(program => program -> parser.parse(program))
+        
+        val failedTests = ranTests.collect {
+            case (program, parsley.Failure(_)) => program
+        }
+        
+        failedTests shouldBe empty 
         }
     }
-    
 }
