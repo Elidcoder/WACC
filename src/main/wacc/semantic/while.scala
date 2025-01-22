4ccb915656ecce39ad8c8f352e2104f4dd2b311f
Serhii Popov <Serhovski@users.noreply.github.com>
2025-01-22 23:00:01 +0000

    feat: semantic check for while statement

--- src/main/wacc/semantic/while.scala
+++ src/main/wacc/semantic/while.scala
@@ -2,4 +2,17 @@ package wacc.semantic
 
 import wacc.ast.* 
 
-def checkSemanticsWhile(e: Expr, s: List[Stmt], env: Environment): (Option[String], Environment) = ???
+def checkSemanticsWhile(e: Expr, s: List[Stmt], env: Environment): (Option[String], Environment) = checkSemanticsExpr(e, env) match {
+    case (Left(err), env)      => checkSemanticsStmts(s, env) match {
+        case (Some(err2), env) => (Some(err + err2), env)
+        case (None,       env) => (Some(err), env)
+    }
+    case (Right(BoolT()), env) => checkSemanticsStmts(s, env) match {
+        case (Some(err2), env) => (Some(err2), env)
+        case (None,      env)  => (None, env)
+    }
+    case (_, env)              => checkSemanticsStmts(s, env) match {
+        case (Some(err2), env) => (Some("CONDITION MUST BE A BOOLEAN EXPRESSION" + err2), env)
+        case (None,      env)  => (Some("CONDITION MUST BE A BOOLEAN EXPRESSION"), env)
+    }
+}
