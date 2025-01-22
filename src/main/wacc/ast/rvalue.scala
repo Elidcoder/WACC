74d946113d1b0dc22231274b2130b25f877653c8
freddien03 <60051731+freddien03@users.noreply.github.com>
2025-01-22 01:34:18 +0000

    fix: add missing functionality for fst and snd Lvalues in AST

--- src/main/wacc/ast/rvalue.scala
+++ src/main/wacc/ast/rvalue.scala
@@ -6,14 +6,26 @@ enum RValue {
     case RExpr(e: Expr)
     case ArrayLit(x: List[Expr])
     case NewPair(e1: Expr, e2: Expr)
-    case PairElem(v: LValue)
+    case PElem(p: PairElem)
     case Call(v: String, x: List[Expr])
 }
+
+enum PairElem {
+    case First(v: LValue)
+    case Second(v: LValue)
+}
+
+
 
 object RValue {
     object RExpr extends ParserBridge1[Expr, RValue]
     object ArrayLit extends ParserBridge1[List[Expr], RValue]
     object NewPair extends ParserBridge2[Expr, Expr, RValue]
-    object PairElem extends ParserBridge1[LValue, RValue]
+    object PElem extends ParserBridge1[PairElem, RValue]
     object Call extends ParserBridge2[String, List[Expr], RValue]
+}
+
+object PairElem {
+    object First extends ParserBridge1[LValue, PairElem]
+    object Second extends ParserBridge1[LValue, PairElem]
 }
