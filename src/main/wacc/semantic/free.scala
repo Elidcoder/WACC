b753262bf8a8a0d2530ab2d26262d2ffe0c44123
Serhii Popov <Serhovski@users.noreply.github.com>
2025-01-22 22:19:05 +0000

    feat: imsantic analysis of free statement

--- src/main/wacc/semantic/free.scala
+++ src/main/wacc/semantic/free.scala
@@ -2,4 +2,24 @@ package wacc.semantic
 
 import wacc.ast.*
 
-def checkSemanticsFree(e: Expr, env: Environment): (Option[String], Environment) = ???
+def checkSemanticsFree(e: Expr, env: Environment): (Option[String], Environment) = e match {
+    case Ident(v: String) => getType(env, v) match {
+        case None             => (Some("VARIABLE IS NOT DECLARED"), env)
+        case Some(ArrayT(_))  => (None, env)
+        case Some(PairT(_,_)) => (None, env)
+        case _                => (Some("INVALID TYPE FOR FREE: MUST BE ARRAY OR PAIR"), env)
+    }
+    case ArrayElem(Ident(v: String), x: List[Expr]) => getType(env, v) match {
+        case None                     => checkSemanticsExprs(x, env) match { 
+            case (None, env)      => (Some("ARRAY IS NOT DECLARED"), env)
+            case (Some(err), env) => (Some("ARRAY IS NOT DECLARED" + err), env)
+        }
+        case Some(ArrayT(ArrayT(_)))  => checkSemanticsExprs(x, env)
+        case Some(ArrayT(PairT(_,_))) => checkSemanticsExprs(x, env)
+        case _                        => checkSemanticsExprs(x, env) match {
+            case (None, env)      => (Some("INVALID TYPE FOR FREE: MUST BE ARRAY OR PAIR"), env)
+            case (Some(err), env) => (Some("INVALID TYPE FOR FREE: MUST BE ARRAY OR PAIR" + err), env)
+        }
+    }
+    case _                            => (Some("INVALID ARGUMENT FOR FREE"), env)
+}
