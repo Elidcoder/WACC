1d226c6cd739acfd294b5cd8538e8eb887cd7c97
freddien03 <60051731+freddien03@users.noreply.github.com>
2025-01-22 01:34:59 +0000

    fix: allow for multiple statements in a function

--- src/main/wacc/ast/lvalue.scala
+++ src/main/wacc/ast/lvalue.scala
@@ -5,11 +5,11 @@ import parsley.generic.{ParserBridge1, ParserBridge2}
 enum LValue {
     case Ident(v: String)
     case ArrayElem(v: String, x: List[Expr])
-    case PairElem(v: LValue)
+    case PElem(v: PairElem)
 }
 
 object  LValue {
     object Ident extends ParserBridge1[String, LValue]
     object ArrayElem extends ParserBridge2[String, List[Expr], LValue]
-    object PairElem extends ParserBridge1[LValue, LValue]
+    object PElem extends ParserBridge1[PairElem, LValue]
 }
