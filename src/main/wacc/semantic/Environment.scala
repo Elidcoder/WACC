144008b36708880d2e01386e79b882450b5c9600
Bruce Chen <bsychen@users.noreply.github.com>
2025-01-23 16:35:11 +0000

    feat: context class that contains a string of errors and a mutable environment

--- src/main/wacc/semantic/Environment.scala
+++ src/main/wacc/semantic/Environment.scala
@@ -2,11 +2,4 @@ package wacc.semantic
 
 import wacc.ast.Type
 
-type Environment = List[List[(String, Type)]]
-
-def contains(env: Environment, v: String): Boolean = ???
-def getType(env: Environment, v: String): Option[Type] = ???
-def removeOption(env: Environment, v: String): Option[Environment] = ???
-def remove(env: Environment, v: String): Environment = ???
-def getScopeFunctionType(env: Environment) = ???
-def getArrayElemType(env: Environment, v: String, n: Int): Option[Type] = ???
+type Environment = Map[String, Type]
