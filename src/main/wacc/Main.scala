1b11645427a32f4e7b1640f340348b5687690880
Bruce Chen <bsychen@users.noreply.github.com>
2025-02-01 21:12:24 +0000

    fix: fix testing pipeline function so that test stage passes

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -1,26 +1,26 @@
 package wacc
 
-import wacc.syntax.parser
-
 import parsley.{Success, Failure}
 import java.io.File
 import wacc.error.*
+import wacc.syntax.parser
 import parsley.errors.ErrorBuilder
 import parsley.errors.tokenextractors.SingleChar
-import scala.util.Random
+
+def pipeline(file: File): Int = {
+    given ErrorBuilder[WaccErr] = new WaccErrorBuilder with SingleChar
+    parser.parse(file) match {
+        case Success(x) => 0
+        case Failure(x) => 100
+    }
+}
 
 def main(args: Array[String]): Unit = {
     args.headOption match {
         case Some(expr) => 
             val file = new File(expr)
             assert(file.exists())
-            parser.parse(file) match {
-                case Success(x) => sys.exit(0)
-                case Failure(msg) => 
-                    print("Syntax error ")
-                    println(msg)
-                    sys.exit(100)
-        }
+            sys.exit(pipeline(file))
         case None => println("please enter an expression")
     }
 }
