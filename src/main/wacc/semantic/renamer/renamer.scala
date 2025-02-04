958d8a0e7aeca010aa1292b1ed2ff294e9f48de0
Serhii Popov <Serhovski@users.noreply.github.com>
2025-02-04 15:30:58 +0000

    fix: redeclearation within scope fails and scope checking for functions

--- src/main/wacc/semantic/renamer/renamer.scala
+++ src/main/wacc/semantic/renamer/renamer.scala
@@ -8,36 +8,45 @@ class QualifiedName(val oldName: String, val uid: Int)
 
 def rename(prog: Program[String, Unit]): (Program[QualifiedName, Unit], Environment) = 
     given env: Environment = new Environment()
-    given mainScope: MutScope = empty
-    val Fs = prog.fs.map(rename(_))
-    val Ss = rename(prog.x, mainScope.toMap)
+    given MutScope = empty
+    given Scope = Map.empty
+    val Fs = renameFuncs(prog.fs)
+    val Ss = rename(prog.x)
     (Program(Fs, Ss)(prog.pos), env)
 
-def rename(f: Func[String, Unit])(using env: Environment, mainScope: MutScope): Func[QualifiedName, Unit] = 
-    val paramMap: MutScope = from((f.l.map { 
-        param => 
-            (param.v.v, env.add(param.v.v, param.pos))
-        }))
-    val F = env.add(f.v.v, f.pos)
-    mainScope.put(f.v.v, F)
-    paramMap.put(f.v.v, F)
+def renameFuncs(fs: List[Func[String, Unit]])(using env: Environment, mainScope: MutScope, parentScope: Scope) = {
+    fs.foreach { f =>
+        mainScope.put(f.v.v, env.add(f.v.v, f.pos))
+    }
+    fs.map(rename(_))
+}
+
+def rename(f: Func[String, Unit])(using env: Environment, mainScope: MutScope, parentScope: Scope): Func[QualifiedName, Unit] = 
+    given funcScope: MutScope = from(mainScope)
+    f.l.foreach {param => 
+        funcScope.put(param.v.v, env.add(param.v.v, param.pos))    
+    }
     Func(
         f.t,
-        paramMap(f.v.v),
-        f.l.map(p => Param(p.t, paramMap(p.v.v))(p.pos)),
-        rename(f.s, paramMap.toMap))(f.pos)
+        funcScope(f.v.v),
+        f.l.map(p => Param(p.t, funcScope(p.v.v))(p.pos)),
+        rename(f.s))(f.pos)
 
-def rename(ss: List[Stmt[String, Unit]], parentScope: Scope)(using Environment): List[Stmt[QualifiedName, Unit]] = {
-    given curScope: MutScope = from(parentScope)
+def rename(ss: List[Stmt[String, Unit]])(using env: Environment, curScope: MutScope, parentScope: Scope): List[Stmt[QualifiedName, Unit]] = {
+    given Scope    = parentScope ++ curScope.toMap
+    given MutScope = empty
     ss.map(rename(_))
 }
 
-def rename(s: Stmt[String, Unit])(using curScope: MutScope, env: Environment): Stmt[QualifiedName, Unit] = s match {
+def rename(s: Stmt[String, Unit])(using curScope: MutScope, env: Environment, parentScope: Scope): Stmt[QualifiedName, Unit] = s match {
     case Skip() => Skip()(s.pos)
-    case NewAss(t, Ident(v), r) => 
-        val i = env.add(v, s.pos);
-        curScope.put(v, i);
-        Assign(i, rename(r))(s.pos)
+    case NewAss(t, i, r) => 
+        val newI = if (curScope.contains(i.v)) 
+            then Ident[QualifiedName, Unit](QualifiedName(i.v, AlreadyDeclaredInScope))(i.pos, ())
+            else env.add(i.v, i.pos)
+        val rR = rename(r)
+        curScope.put(i.v, newI)
+        Assign(newI, rR)(s.pos)
     case Assign(l, r) => Assign(rename(l), rename(r))(s.pos)
     case Read(l) => Read(rename(l))(s.pos)
     case Free(e) => Free(rename(e))(s.pos)
@@ -45,19 +54,19 @@ def rename(s: Stmt[String, Unit])(using curScope: MutScope, env: Environment): S
     case Exit(e) => Exit(rename(e))(s.pos)
     case Print(e) => Print(rename(e))(s.pos)
     case PrintLn(e) => PrintLn(rename(e))(s.pos)
-    case If(e, s1s, s2s) => If(rename(e), rename(s1s, curScope.toMap), rename(s2s, curScope.toMap))(s.pos)
-    case While(e, ss) => While(rename(e), ss.map(rename(_)))(s.pos)
-    case Nest(ss) => Nest(ss.map(rename(_)))(s.pos)
+    case If(e, s1s, s2s) => If(rename(e), rename(s1s), rename(s2s))(s.pos)
+    case While(e, ss) => While(rename(e), rename(ss))(s.pos)
+    case Nest(ss) => Nest(rename(ss))(s.pos)
 }
 
-def rename(l: LValue[String, Unit])(using env: Environment, curScope: MutScope): LValue[QualifiedName, Unit] = l match {
+def rename(l: LValue[String, Unit])(using env: Environment, curScope: MutScope, parentScope: Scope): LValue[QualifiedName, Unit] = l match {
     case i: Ident[String, Unit] => curScope.rebuildWithIdent(i)(identity(_))
     case ArrayElem(i, x) => curScope.rebuildWithIdent(i)(ArrayElem(_, x.map(rename(_)))(l.pos))
     case PElem(First(l)) => rename(l)
     case PElem(Second(l)) => rename(l)
 }
 
-def rename(r: RValue[String, Unit])(using env: Environment, curScope: MutScope): RValue[QualifiedName, Unit] = r match {
+def rename(r: RValue[String, Unit])(using env: Environment, curScope: MutScope, parentScope: Scope): RValue[QualifiedName, Unit] = r match {
     case e: Expr[String, Unit] => rename(e)
     case ArrayLit(es) => ArrayLit(es.map(rename(_)))(r.pos, ())
     case NewPair(e1, e2) => NewPair(rename(e1), rename(e2))(r.pos, ())
@@ -68,7 +77,7 @@ def rename(r: RValue[String, Unit])(using env: Environment, curScope: MutScope):
     case PElem(s: Second[String, Unit]) => PElem(Second(rename(s.v))(s.pos, ()))(r.pos, ())
 }
 
-def rename(e: Expr[String, Unit])(using env: Environment, curScope: MutScope): Expr[QualifiedName, Unit] = e match {
+def rename(e: Expr[String, Unit])(using env: Environment, curScope: MutScope, parentScope: Scope): Expr[QualifiedName, Unit] = e match {
     case Not(e) => Not(rename(e))(e.pos)
     case Neg(e) => Neg(rename(e))(e.pos)
     case Len(e) => Len(rename(e))(e.pos)
@@ -96,10 +105,10 @@ def rename(e: Expr[String, Unit])(using env: Environment, curScope: MutScope): E
     case ArrayElem(i, es) => curScope.rebuildWithIdent(i)(ArrayElem(_, es.map(rename(_)))(e.pos))
 }
 
-extension (scope: MutScope) 
+extension (curScope: MutScope) 
     def rebuildWithIdent[A](id: Ident[String, Unit])
     (build: Ident[QualifiedName, Unit] => A)
-    (using curScope: MutScope): A = curScope.get(id.v) match {
-        case Some(i) => build(i)
-        case None    => build(Ident(QualifiedName(id.v, Undeclared))(id.pos, ()))
-    }
+    (using parentScope: Scope): A = 
+        build(curScope.get(id.v)
+        .getOrElse(parentScope.get(id.v)
+        .getOrElse(Ident[QualifiedName, Unit](QualifiedName(id.v, Undeclared))(id.pos, ()))))
