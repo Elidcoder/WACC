57a35feabd738a6f1456d3f9ee37c04c532c07a4
Bruce Chen <bsychen@users.noreply.github.com>
2025-02-06 13:10:00 +0000

    refactor: remove redundant catching of exit code in shell as test job passes due to ignore

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# trap SIGTERM and exit gracefully
-trap 'echo "Job cancelled - exiting gracefully"; exit 0' SIGTERM
-
 usage() {
     echo "Usage: $0 [all|syntax|valid|semantic]"
     exit 1
