e8f09c31cb73acaf689d6d5c2651fd405e49543b
Eli Gilbert <Elidcoder@users.noreply.github.com>
2025-02-24 15:58:08 +0000

    feat(ir): implement the instruction trait and instances for some intructions (eg1923, sp3123, bc522, fn122)

---
+++ src/main/wacc/backend/IR/Block.scala
@@ -0,0 +1,6 @@
+package wacc.backend.ir
+
+class Block(val funcName: String, val instrs: List[Instr]) { 
+    // TODO()
+    def format():String = ""
+}
--- src/main/wacc/backend/IR/instructions.scala
+++ src/main/wacc/backend/IR/instructions.scala
@@ -2,5 +2,18 @@ package wacc.backend.ir
 
 sealed trait Operand
 
+sealed trait Instr
+
 case class Reg(reg: Register, size: RegisterSize) extends Operand
 
+case class Push(source: Operand) extends Instr
+case class Pop(dest: Operand) extends Instr
+case class Label(name: String) extends Instr
+case class Call(funcName: String) extends Instr
+
+case class Add(dest: Operand, opR: Operand) extends Instr
+case class Sub(dest: Operand, opR: Operand) extends Instr
+case class Cmp(dest: Operand, opR: Operand) extends Instr
+
+case class Mov(source: Operand, dest: Operand) extends Instr
+case class Lea(dest: Operand, target: Operand) extends Instr
