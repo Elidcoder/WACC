aa047f1a251cda47d7f4b6174a3ef2cd0cca2091
Serhii Popov <Serhovski@users.noreply.github.com>
2025-02-26 19:08:06 +0000

    feat(prebuiltGenerator): divZero and overflow blocks (sp3123)

--- src/main/wacc/backend/generator/prebuiltGenerator.scala
+++ src/main/wacc/backend/generator/prebuiltGenerator.scala
@@ -10,10 +10,10 @@ object prebuiltGenerator {
             IMov(Reg[QWORD](rbp), Reg[QWORD](rsp)),
             IAnd(Reg[QWORD](rsp), Imm[QWORD](-16)),
             ICall("exit@plt"),
-            IMov(Reg[QWORD](rsp), Reg[QWORD](rbp))
+            IMov(Reg[QWORD](rsp), Reg[QWORD](rbp)),
+            IRet
         )
     )
-    
     val printBlock: Block = Block (
         Label("_prints"),
         Some(List(RoData(4, "%.*s", Label(".L._prints_str0")))),
@@ -23,28 +23,52 @@ object prebuiltGenerator {
             IAnd(Reg[QWORD](rsp), Imm[QWORD](-16)),
             IMov(Reg[QWORD](rdx), Reg[QWORD](rdi)),
             IMov(Reg[DWORD](rsi), MemOff[DWORD](rdi, -4)),
-            ILea(Reg[QWORD](rdi), Rip[QWORD](".L._prints_str0")),
+            ILea(Reg[QWORD](rdi), Rip[QWORD](Label(".L._prints_str0"))),
             IMov(Reg[BYTE](rax), Imm[BYTE](0)),
             ICall("printf@plt"),
             IMov(Reg[QWORD](rdi), Imm[QWORD](0)),
             ICall("fflush@plt"),
             IMov(Reg[QWORD](rsp), Reg[QWORD](rbp)),
-            IPop(Reg[QWORD](rbp))
+            IPop(Reg[QWORD](rbp)),
+            IRet
         )
     )
     val printlnBlock = Block (
         Label("_println:"),
-        Some(List(RoData(0, ".L._println_str0", ""))),
-        List (
+        Some(List(RoData(0, "", Label(".L._println_str0")))),
+        List(
             IPush(Reg[QWORD](rbp)),
             IMov(Reg[QWORD](rbp), Reg[QWORD](rsp)),
             IAnd(Reg[QWORD](rsp), Imm[QWORD](-16)),
-            ILea(Reg[QWORD](rdi), Rip[QWORD](".L._println_str0")),
+            ILea(Reg[QWORD](rdi), Rip[QWORD](Label(".L._println_str0"))),
             ICall("puts@plt"),
             IMov(Reg[QWORD](rdi), Imm[QWORD](0)),
             ICall("fflush@plt"),
             IMov(Reg[QWORD](rsp), Reg[QWORD](rbp)),
-            IPop(Reg[QWORD](rbp))
+            IPop(Reg[QWORD](rbp)),
+            IRet
+        )
+    )
+    val overflowBlock = Block (
+        Label("_errOverflow"),
+        Some(List(RoData(52, "fatal error: integer overflow or underflow occurred\n", Label(".L._errOverflow_str0")))),
+        List(
+            IAnd(Reg[QWORD](rsp), Imm[QWORD](-16)),
+            ILea(Reg[QWORD](rdi), Rip[QWORD](Label(".L._errOverflow_str0"))),
+            ICall("_prints"),
+            IMov(Reg[BYTE](rdi), Imm[BYTE](-1)),
+            ICall("exit@plt")
+        )
+    )
+    val divZeroBlock = Block (
+        Label("_errDivZero"),
+        Some(List(RoData(40, "fatal error: division or modulo by zero\n", Label(".L._println_str0")))),
+        List(
+            IAnd(Reg[QWORD](rsp), Imm[QWORD](-16)),
+            ILea(Reg[QWORD](rdi), Rip[QWORD](Label(".L._errDivZero_str0"))),
+            ICall("_prints"),
+            IMov(Reg[BYTE](rdi), Imm[BYTE](-1)),
+            ICall("exit@plt")
         )
     )
 }
