05f071c2fedb056c1be0a7d236932738810207c9
Bruce Chen <bsychen@users.noreply.github.com>
2025-02-26 19:30:22 +0000

    feat(generator): finish framework for conditional jumping and labelling in while/if statements (bc522)

--- src/main/wacc/backend/generator/generator.scala
+++ src/main/wacc/backend/generator/generator.scala
@@ -9,6 +9,8 @@ import scala.collection.mutable.Builder
 
 final val RETURN_REG       = rax
 final val TEMP_REG         = r10
+final val BASE_PTR_REG     = rbp
+final val STACK_PTR_REG    = rsp
 final val FIRST_PARAM_REG  = rdi
 final val SECOND_PARAM_REG = rsi
 final val THIRD_PARAM_REG  = rdx
@@ -16,12 +18,21 @@ final val FOURTH_PARAM_REG = rcx
 final val FIFTH_PARAM_REG  = r8
 final val SIXTH_PARAM_REG  = r9
 
+final val REMAINDER_REG = rdx
+
 object generator {
     def generate(prog: Program[QualifiedName, KnownType])(using ctx: Context): List[Block] = {
         val blockBuilder = List.newBuilder[Block]
         // TODO: change roData for main
         val mainBuilder = List.newBuilder[Instr]
+        mainBuilder
+            += IPush (Reg[QWORD] (BASE_PTR_REG))
+            += IMov (Reg[QWORD] (BASE_PTR_REG), Reg[QWORD] (STACK_PTR_REG))
         generateStmts(prog.stmts, mainBuilder)
+        mainBuilder
+            += IMov (Reg[DWORD] (RETURN_REG), Imm[DWORD] (0))
+            += IPush (Reg[QWORD] (BASE_PTR_REG))
+            += IRet
         blockBuilder += Block(Label ("main"), None, mainBuilder.result())
         prog.funcs.foreach { func => blockBuilder += generate(func) }
         // ctx.getPrebuilts().foreach { prebuilt => builder += generatePrebuiltBlock(prebuilt) }
@@ -31,6 +42,9 @@ object generator {
     def generate(func: Func[QualifiedName, KnownType])(using ctx: Context): Block = {
         // TODO: roData for function?
         val builder = List.newBuilder[Instr]
+        builder
+            += IPush (Reg[QWORD] (BASE_PTR_REG))
+            += IMov (Reg[QWORD] (BASE_PTR_REG), Reg[QWORD] (STACK_PTR_REG))
         generateStmts(func.stmts, builder)
         Block(Label (func.id.name.oldName), None, builder.result())
     }
@@ -56,11 +70,11 @@ object generator {
             += IPush (Reg[DWORD] (RETURN_REG)) 
             += IPop (Reg[DWORD] (TEMP_REG))
             += IPop (Reg[DWORD] (RETURN_REG))
-            += build((Reg[DWORD] (RETURN_REG)), (Reg[DWORD] (TEMP_REG)))
+            += build((Reg[DWORD] (RETURN_REG)), (Reg[DWORD] (REMAINDER_REG)))
             // TODO: check for overflow
     }
 
-    def generateDiv(
+    def generateDivMod(
         left: Expr[QualifiedName, KnownType], 
         right: Expr[QualifiedName, KnownType],
         builder: Builder[Instr, List[Instr]]
@@ -79,6 +93,33 @@ object generator {
             += IDiv (Reg[DWORD] (TEMP_REG))
     }
 
+    def generateBinCond(
+        left: Expr[QualifiedName, KnownType],
+        right: Expr[QualifiedName, KnownType],
+        cond: JumpCond,
+        builder: Builder[Instr, List[Instr]],
+    )(using ctx: Context): Unit = {
+        generate(left, builder) 
+        builder 
+            += IPush (Reg[QWORD] (RETURN_REG)) 
+        generate(right, builder)
+        builder 
+            += IPush (Reg[QWORD] (RETURN_REG))
+            += IPop (Reg[QWORD] (TEMP_REG))
+            += IPop (Reg[QWORD] (RETURN_REG))
+            += ICmp (Reg[QWORD] (RETURN_REG), Reg[QWORD] (TEMP_REG))
+            += (cond match {
+                    case JumpCond.E  => ???
+                    case JumpCond.NE => ???
+                    case JumpCond.G  => ???
+                    case JumpCond.GE => ???
+                    case JumpCond.L  => ???
+                    case JumpCond.LE => ???
+                    case JumpCond.UnCond => ???
+                }
+            )
+    }
+
     def generate(
         expr: Expr[QualifiedName, KnownType],
         builder: Builder[Instr, List[Instr]]
@@ -88,7 +129,26 @@ object generator {
             case Add(x, y) => generateAddSubMul(x, y, IAdd.apply, builder)
             case Sub(x, y) => generateAddSubMul(x, y, ISub.apply, builder)
             case Mul(x, y) => generateAddSubMul(x, y, IMul.apply, builder)
-            case Div(x, y) => generateDiv(x, y, builder)
+            case Div(x, y) => generateDivMod(x, y, builder)
+            case Mod(x, y) => generateDivMod(x, y, builder)
+                builder
+                    += IMov (Reg[QWORD] (RETURN_REG), Reg[QWORD] (TEMP_REG))
+            case Eq(left, right) => 
+                generateBinCond(left, right, JumpCond.E, builder)
+            case NotEq(left, right) => 
+                generateBinCond(left, right, JumpCond.NE, builder)
+            case Greater(left, right) => 
+                generateBinCond(left, right, JumpCond.G, builder)
+            case GreaterEq(left, right) =>
+                generateBinCond(left, right, JumpCond.GE, builder)
+            case Less(left, right) =>
+                generateBinCond(left, right, JumpCond.L, builder)
+            case LessEq(left, right) =>
+                generateBinCond(left, right, JumpCond.LE, builder) 
+            case BoolLit(bool) =>
+                builder += 
+                    IMov (Reg[BYTE] (RETURN_REG), (Imm (if (bool) 1 else 0)))
+                    ICmp (Reg[BYTE] (RETURN_REG), Imm (1))
             case _         => ??? // TODO
         }
         builder.result()
@@ -107,13 +167,48 @@ object generator {
     )(using ctx: Context): Unit = {
         stmt match {
             case Skip() => ()
+            case NewAss(assType, id, rVal) => ??? // TODO
+            case Assign(lVal, rVal) => ???
+            case Read(lVal) => ???
+            case Free(expr) => ???
+            case Return(expr) =>
+                generate(expr, builder)
+                builder
+                    += IMov (Reg[QWORD] (STACK_PTR_REG), Reg[QWORD] (BASE_PTR_REG))
+                    += IPop (Reg[QWORD] (BASE_PTR_REG))
+                    += IRet
             case Exit(expr) => 
                 generate(expr, builder)
                 builder
                     += IMov (Reg[DWORD] (FIRST_PARAM_REG), (Reg[DWORD] (RETURN_REG)))
                     += ICall ("_exit") // TODO: replace _exit string with prebuilt attrib
-            case NewAss(assType, id, rVal) => ??? // TODO
-            case _                         => ??? // TODO
+            case Print(expr) => ???
+            case PrintLn(expr) => ???
+            case If(cond, ifStmts, elseStmts) => 
+                val (ifLabel, endLabel) = (ctx.nextLabel(), ctx.nextLabel())
+                generate(cond, builder)
+                builder
+                    += Jmp (ifLabel, JumpCond.E)
+                generateStmts(elseStmts, builder)
+                builder
+                    += Jmp (endLabel, JumpCond.E)
+                    += ifLabel
+                generateStmts(ifStmts, builder)
+                builder
+                    += endLabel
+            case While(cond, stmts) =>
+                val (condLabel, bodyLabel) = (ctx.nextLabel(), ctx.nextLabel())
+                builder
+                    += Jmp (condLabel, JumpCond.UnCond)
+                    += bodyLabel
+                generateStmts(stmts, builder)
+                builder
+                    += condLabel
+                generate(cond, builder)
+                builder
+                    += Jmp (bodyLabel, JumpCond.E)
+            case Nest(stmts) => 
+                generateStmts(stmts, builder)
         }
     }
 
