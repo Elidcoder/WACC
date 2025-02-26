34f1bb7144f774d3dc0c3c57e3a28f92cfb5418b
Eli Gilbert <Elidcoder@users.noreply.github.com>
2025-02-26 18:29:35 +0000

    feat(backend): implement rodata extraction in referencer (eg1923)

--- src/main/wacc/backend/referencer/referencer.scala
+++ src/main/wacc/backend/referencer/referencer.scala
@@ -16,8 +16,11 @@ case class PbFree( varType: KnownType) extends Prebuilt
 case class PbRead( varType: KnownType) extends Prebuilt
 
 object referencer {
+    /* An ordered list of registers used for parameters. */
     private val parameterRegisters: List[Register] = List(rdi, rsi, rdx, rcx, r8, r9) 
 
+    /* Returns the dataSize matching a given type, 
+     * we have a static guarantee from frontend _ never occurs. */
     private def getTypeSize(usedType: Type): DataSize = usedType match {
         case IntT()          => DWORD()
         case CharT()         => BYTE()
@@ -29,6 +32,7 @@ object referencer {
         case _               => QWORD()
     }
 
+    /* Creates a Reg of the size matching the given ty and the given register. */
     private def buildReg(reg: Register, ty: Type): Reg[DataSize] = ty match {
         case IntT()          => Reg[DWORD](reg)
         case CharT()         => Reg[BYTE](reg)
@@ -46,6 +50,7 @@ object referencer {
         ctx.incFuncOff(funcName, getTypeSize(id.t).bytes)
     }
 
+    /* Reference the variables used in a program as well as storing any strings. */
     def reference(prog: Program[QualifiedName, KnownType])(using ctx: Context): Program[QualifiedName, KnownType] = {
         given funcName:QualifiedName = QualifiedName("main", -1)
 
@@ -59,6 +64,7 @@ object referencer {
         prog
     }
     
+    /* Reference the variables used in a function as well as its parameters. */
     private def reference(func: Func[QualifiedName, KnownType])(using ctx: Context): Unit  = {
         given funcName:QualifiedName = func.id.name
 
@@ -77,17 +83,39 @@ object referencer {
         reference(func.stmts)
     }
 
-    private def reference(funcStmts: List[Stmt[QualifiedName, KnownType]])(using ctx: Context, funcName: QualifiedName): Unit
-        = funcStmts.foreach(reference)
+    /* Call reference on all stmts in a list of stmts. */
+    private def reference(stmts: List[Stmt[QualifiedName, KnownType]])(using ctx: Context, funcName: QualifiedName): Unit
+        = stmts.foreach(reference)
 
+    /* Reference the variables used in a stmt as well as storing any strings. */
     private def reference(stmt: Stmt[QualifiedName, KnownType])(using ctx: Context, funcName: QualifiedName): Unit = stmt match {
-        case NewAss(_, id, _) => addVarToContext(id)
+        case NewAss(_, id, rval) => {
+            addVarToContext(id)
+            searchForStrings(rval)
+        }
+        case Assign(_, rVal) => searchForStrings(rVal) 
 
         /* Check nested statements. */
         case If(cond, ifStmts, elseStmts) => {reference(ifStmts); reference(elseStmts)}
         case While(cond, subStmts) => reference(subStmts)
         case Nest(stmts) => reference(stmts)
 
+        case _ => {}
+    }
+
+    /* Search through a rValue. recursively looking for strings. */
+    private def searchForStrings(rval: RValue[QualifiedName, KnownType])(using ctx: Context, funcName: QualifiedName): Unit = rval match {
+        case Eq(lhsExpr, rhsExpr) => {
+            searchForStrings(lhsExpr)
+            searchForStrings(rhsExpr)
+        }
+        case NotEq(lhsExpr, rhsExpr) => {
+            searchForStrings(lhsExpr)
+            searchForStrings(rhsExpr)
+        }
+        case StrLit(str) => {
+            ctx.addRoData(str, ctx.nextStringLabel())
+        }
         case _ => {}
     }
 }
